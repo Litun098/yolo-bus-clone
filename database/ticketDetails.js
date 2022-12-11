d1cec4b672a84eaca20077434200be7d53417804
Litun098 <nayaklitun9@gmail.com>
2022-12-11 21:25:24 +0530

    indent

--- database/ticketDetails.js
+++ database/ticketDetails.js
@@ -2,33 +2,33 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ticketSchema = new Schema({
-    status:{
-        type:Boolean
+    status: {
+        type: Boolean
     },
-    ticketNo:{
-        type:Number,
-        unique:true,
-        sparse:true,
-        min:1,
-        max:40
+    ticketNo: {
+        type: Number,
+        unique: true,
+        sparse: true,
+        min: 1,
+        max: 40
     },
-    userDetail:[{
-        name:{
-            type:String
+    userDetail: [{
+        name: {
+            type: String
         },
-        age:{
-            type:Number
+        age: {
+            type: Number
         }
     }],
-    sourse:{
-        type:String
+    sourse: {
+        type: String
     },
-    destination:{
-        type:String
+    destination: {
+        type: String
     }
 });
 
-const ticketModel = mongoose.Model('tickets',ticketSchema);
+const ticketModel = mongoose.Model('tickets', ticketSchema);
 module.exports = {
     ticketModel
 }
